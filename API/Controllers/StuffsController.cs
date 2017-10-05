0417ab9fb3eab56772425808277d6c7bd16ccdce
kcoleman <kcoleman@springthrough.com>
2017-10-04 23:57:46 -0400

    initial api commit

---
+++ API/Controllers/StuffsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class StuffsController : ApiController
+    {
+        /// <summary>
+        /// Gets the stuffs.
+        /// </summary>
+        [HttpGet]
+        [Route("stuffs")]
+        public void GetStuffs()
+        {
+
+        }
+    }
+}
